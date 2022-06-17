390fc7718cb0b9ea45a1c27d8a595e16daa44260
amaljom <amaljom99@gmail.com>
2022-06-17 17:17:47 +0200

    completato secondo esercizio coi console.log, provvederò a mandare tutto in output dopo

--- js/script.js
+++ js/script.js
@@ -4,24 +4,27 @@
 // ! IN BASE AL FATTO CHE TROVI O NO UN ELEMENTO DELL ARRAY CHE VI CORRISPONDE MANDERMO IN OUTPUT
 // ! L' ESITO PER LA RICHIESTA DEL UTENTE DI ENTRARE 
 
-const emailUser= prompt(" inserisci la tua email se desideri effettuare l'accesso'");
-console.log(emailUser);
+// ? per dopo const emailData=document.getElementById('data-acquisition');
+// ? el.addEventListener("click", modifyText, false);
 
-const emailList = ['ciccio@gmail.com', 'mario@gmail.com', 'ermengildo@gmail.com'];
-console.log(emailList);
-let trovato=false;
+// * const emailUser= prompt(" inserisci la tua email se desideri effettuare l'accesso'");
+// * console.log(emailUser);
 
- for (let i = 0; i < emailList.length ; i++) {
-     if(emailUser==emailList[i]){
-        trovato=true;
-     }  
- }
- if(trovato==true){
-    console.log('puoi entrare!');
- }
- else{
-    console.log('non risulti presente nel database')
- }
+// * const emailList = ['ciccio@gmail.com', 'mario@gmail.com', 'ermengildo@gmail.com'];
+// * console.log(emailList);
+// * let trovato=false;
+
+// *  for (let i = 0; i < emailList.length ; i++) {
+// *      if(emailUser==emailList[i]){
+// *         trovato=true;
+// *      }  
+// *  }
+// *  if(trovato==true){
+// *     console.log('puoi entrare!');
+// *  }
+// *  else{
+// *     console.log('non risulti presente nel database')
+// *  }
 // ! possibile altra risuluzione dell'esercizio della mail
 // let risultato = emailList.indexOf(emailUser);
 // console.log(risultato);
@@ -31,3 +34,21 @@ let trovato=false;
 // else{
 //     console.log('non risulti presente nel database');
 // }
+
+
+// * SECONDO ESERCIZIO //
+
+let numeroUtente = Math.floor((Math.random() * 6)+1);
+console.log(numeroUtente);
+
+let numeroCalcolatore = Math.floor((Math.random() * 6)+1);
+console.log(numeroCalcolatore);
+
+if(numeroUtente == numeroCalcolatore){
+   console.log('siete entrambi fortunati!');
+}
+else if(numeroUtente > numeroCalcolatore){
+   console.log("l'utente ha vinto!");
+}else if(numeroUtente < numeroCalcolatore){
+   console.log('il computer ha vinto!');
+}
